c547182c98d2e5bb804731766250025577c2f44b
andriitaran-psa <andrii.taran@gov.bc.ca>
2023-12-21 11:49:39 -0800

    content updates for Nomination Form

---
+++ src/services/organizations: [.js
@@ -0,0 +1,58 @@
+organizations: [
+  {
+    key: "org-1",
+    label: "Post-Secondary Education and Future Skills",
+  },
+  { key: "org-2", label: "Agriculture and Food" },
+  { key: "org-3", label: "Attorney General" },
+  { key: "org-4", label: "Children and Family Development" },
+  { key: "org-5", label: "Citizens’ Services" },
+  { key: "org-6", label: "Education and Child Care" },
+  {
+    key: "org-7",
+    label: "Emergency Management and Climate Readiness",
+  },
+  {
+    key: "org-8",
+    label: "Energy, Mines and Low Carbon Innovation",
+  },
+  {
+    key: "org-9",
+    label: "Environment and Climate Change Strategy",
+  },
+  { key: "org-10", label: "Assessment Office" },
+  { key: "org-11", label: "Finance" },
+  {
+    key: "org-12",
+    label: "Forests",
+  },
+  { key: "org-13", label: "Health" },
+  {
+    key: "org-14",
+    label: "Indigenous Relations and Reconciliation",
+  },
+  {
+    key: "org-15",
+    label: "Jobs, Economic Development and Innovation",
+  },
+  { key: "org-16", label: "Labour" },
+  { key: "org-17", label: "Mental Health and Addictions" },
+  { key: "org-18", label: "Municipal Affairs" },
+
+  { key: "org-19", label: "Public Safety and Solicitor General" },
+  {
+    key: "org-20",
+    label: "Social Development & Poverty Reduction",
+  },
+  { key: "org-21", label: "Tourism, Arts, Culture and Sport" },
+  { key: "org-22", label: "Transportation & Infrastructure" },
+  { key: "org-23", label: "BC Public Service Agency" },
+  { key: "org-24", label: "Government Communications and Public Engagement" },
+  { key: "org-25", label: "Office of the Premier" },
+  { key: "org-26", label: "Water, Land and Resource Stewardship" },
+  { key: "org-27", label: "Housing" },
+  {
+    key: "org-28",
+    label: "Declaration Act Secretariat",
+  },
+];
