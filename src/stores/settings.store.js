13c589c0740b4b3461e8377754d9037e261de5ed
paulbothma-govbcca <paul.bothma@gov.bc.ca>
2024-10-04 10:37:05 -0700

    Updated getMaxWordCounts to check if total is an object

--- src/stores/settings.store.js
+++ src/stores/settings.store.js
@@ -118,7 +118,7 @@ export const settingsStore = defineStore({
     },
     /*
       This function returns a Proxy object and then creates a watch on the .items array. When the items have finally loaded the watch callback performs the lookup again and updates the value.
-      Banging your head against a wall because you can't figure out why a setting has changed? That is because the proxy/ref being returned is probably changed somewhere down the line.
+      Take note that a setting can be changed unexpectedly because the proxy/ref being returned is probably changed somewhere down the line.
     */
     lookup(type, key, fullValue) {
       if (this.items == undefined || this.items.length == 0) {
